2fccd609a5ed449859380dd0132858af1ca6be98
Xiao G. Wu <xiaogwu@gmail.com>
2013-07-10 16:09:24 -0700

    Finished app

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,18 @@ class QuotesController < ApplicationController
 
   def quote
     @symbol = params[:symbol].upcase
-    @quote = YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)
+    if @symbol.empty?
+      flash[:error] = "Symbol can't be blank.  Please try again."
+      # redirect_to '/'
+      render :home
+    else
+      @quote = YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)
+      if @quote[@symbol].lastTrade == 0
+        flash[:error] = "Invalid symbol.  Please try again."
+        # redirect_to '/'
+        render :home
+      end
+    end
   end
 
 end
